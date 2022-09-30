dd61d86bef4d2da68d70b200f60dfd14065249cd
Tamas Norbert Varga <vargatn@gmail.com>
2022-09-30 14:44:44 +0200

    fixed missing WCS in shear class

---
+++ synthetic/render/frame.py
@@ -0,0 +1,73 @@
+
+import numpy as np
+import subprocess
+import fitsio as fio
+import galsim
+
+from . import shear
+from . import render
+
+
+
+class Frame(object):
+    def __init__(self, catalog, name="canvas", canvas_size=5000, band="i", noise_std=8.36, config_se="config.sex", pixel_scale=0.264):
+        self.catalog = catalog
+        self.canvas_size = canvas_size
+        self.band = band
+        self.noise_std = noise_std
+        self.name = name
+        self.config_se = config_se
+        self.pixel_scale = pixel_scale
+
+    def render(self, nprocess=100):
+        self.df = render.DrawField(self.canvas_size, self.catalog, band=self.band)
+        self.df.prepare()
+        self.df.make_infodicts()
+        self.df.multi_render(nprocess)
+        self.df.collate_stamps()
+        self.canvas = self.df.canvas
+
+        self.noise = np.random.normal(scale=self.noise_std, size=(self.canvas_size, self.canvas_size))
+        self.canvas += self.noise
+
+        self.write()
+
+    def write(self):
+        self.file_name = self.name + ".fits"
+        fio.write(self.file_name, self.canvas.array, clobber=True)
+        self.file_name_psf = self.name + "_epsf.fits"
+        fio.write(self.file_name_psf, self.df.image_epsf.array, clobber=True)
+
+    def extract(self):
+
+        self.file_name = self.name + ".fits"
+        self.catalog_name = self.name + "_cat.fits"
+        self.seg_name = self.name + "_seg.fits"
+
+        cmd = "sex " + self.name + ".fits -c " + self.config_se + " -CATALOG_NAME " + self.catalog_name + " -CHECKIMAGE_NAME " + self.seg_name
+        pp = subprocess.Popen(cmd.split(" "), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = pp.communicate()
+
+        self.scat = fio.read(self.catalog_name)
+        self.seg = fio.read(self.seg_name)
+
+    def ksb(self):
+        self.file_name = self.name + ".fits"
+        self.catalog_name = self.name + "_cat.fits"
+        self.seg_name = self.name + "_seg.fits"
+
+        self.scat = fio.read(self.catalog_name)
+        self.seg = fio.read(self.seg_name)
+        self.file_name_psf = self.name + "_epsf.fits"
+        self.epsf = fio.read(self.file_name_psf)
+
+        canvas = fio.read(self.file_name)
+        self.canvas = galsim.ImageF(canvas)
+
+        self.ids = self.scat['NUMBER']
+        self.cens = np.vstack((self.scat['X_IMAGE'], self.scat['Y_IMAGE'])).T
+        self.sizes = np.around(self.scat["FWHM_IMAGE"] * 2)
+        self.sizes = np.max((np.zeros(len(self.sizes)) + 8, self.sizes), axis=0)
+        self.sc = shear.Shear(self.canvas, self.epsf, self.seg)
+        self.sc.extract_stamps(self.cens, imasks=self.ids, sizes=self.sizes)
+        self.sc.estimate_shear(sky_var=self.noise_std**2)
---
+++ synthetic/render/shear.py
@@ -0,0 +1,76 @@
+import galsim
+import numpy as np
+
+
+class Shear(object):
+    def __init__(self, canvas, image_epsf, maskmap, pixel_scale):
+        """ITs very important for the images and stamps and psf images to have a proper image format, includeing WCS"""
+        self.canvas = canvas.copy()
+        self.image_epsf = galsim.ImageF(image_epsf, wcs=galsim.PixelScale(pixel_scale))
+        self.BADVAL = -9999
+        self.maskmap = galsim.ImageF(maskmap, wcs=galsim.PixelScale(pixel_scale)).copy()
+        self.pixel_scale = pixel_scale
+
+    def extract_stamps(self, centers, imasks, sizes):
+
+        self.stamps = []
+        self.masks = []
+        for i in np.arange(len(centers)):
+            half_size = sizes[i] // 2 + 2
+            cen = centers[i]
+            bb = galsim.bounds.BoundsI(np.round(cen[0]) - half_size,
+                                       np.round(cen[0]) + half_size - 1,
+                                       np.round(cen[1]) - half_size,
+                                       np.round(cen[1]) + half_size - 1)
+
+            stamp = self.canvas[bb].array.copy()
+            mask = self.maskmap[bb].array.copy()
+            mm = (mask != imasks[i]) & (mask != 0)
+            muniques = np.unique(mask)
+            stamp[mm] = 0
+            mask[mm] = 0
+
+            self.masks.append(self.maskmap[bb].array.copy())
+            self.stamps.append(galsim.ImageF(stamp, wcs=galsim.PixelScale(self.pixel_scale)))
+
+    def estimate_shear(self, sky_var=0, shear_est="KSB"):
+        """
+        shear modes = REGAUSS’, ‘LINEAR’, ‘BJ’, or ‘KSB’
+        which is raw or canvas, default to canvas
+        """
+
+        #         stamps = self.raw_stamps
+        # if which == "canvas":
+        stamps = self.stamps
+
+        self.logs = []
+        self.shears = []
+        self.success = []
+        self.fluxes = []
+        self.shears_error = []
+        for i, im in enumerate(stamps):
+            try:
+                res = galsim.hsm.EstimateShear(im, self.image_epsf, shear_est=shear_est, sky_var=sky_var)
+                #                 print(res)
+                self.logs.append(res)
+                self.success.append(True)
+
+                tmp = np.array([res.corrected_g1, res.corrected_g2])
+
+                self.shears.append(tmp)
+                self.fluxes.append(res.moments_amp * (self.pixel_scale) ** 2)
+                self.shears_error.append(res.corrected_shape_err)
+            #                             print("here")
+            except:
+                self.success.append(False)
+                self.logs.append(None)
+                self.fluxes.append(None)
+                tmp = np.array([self.BADVAL, self.BADVAL])
+                self.shears.append(tmp)
+                self.shears_error.append(None)
+
+        self.fluxes = np.array(self.fluxes)
+        self.shears = np.array(self.shears)
+        self.success = np.array(self.success)
+        self.shears_error = np.array(self.shears_error)
+
