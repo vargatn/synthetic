5e0818abf396ed267101032ac370735f13cf6467
Tamas Norbert Varga <vargatn@gmail.com>
2023-02-17 14:32:43 +0100

    updated install with auto yes

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ conda create -n synth python=3.9 anaconda
 
 conda activate synth
 
-conda install -c conda-forge ngmix=1.3.9 # this is a really complex package, if this installs, probably everything else works
-conda install -c conda-forge fitsio galsim healpy esutil
+yes | conda install -c conda-forge ngmix=1.3.9 # this is a really complex package, if this installs, probably everything else works
+yes | conda install -c conda-forge fitsio galsim healpy esutil
 
 pip install git+https://github.com/esheldon/images.git \
             git+https://github.com/esheldon/meds \
