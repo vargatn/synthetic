fde44cb036ac62750ed6a0385bdad3697b41d0a5
Tamas Norbert Varga <vargatn@gmail.com>
2023-02-17 14:57:30 +0100

    updated install script

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+# this script must be run in interactive mode
+# bash -i install.sh
 
 conda create -y -n synth python=3.9 anaconda
 
 conda activate synth
+
+conda env list
 
 conda install -y -c conda-forge ngmix=1.3.9 # this is a really complex package, if this installs, probably everything else works
 conda install -y -c conda-forge fitsio galsim healpy esutil
@@ -10,3 +14,6 @@ conda install -y -c conda-forge fitsio galsim healpy esutil
 pip install git+https://github.com/esheldon/images.git \
             git+https://github.com/esheldon/meds \
             git+https://github.com/esheldon/psfex.git \
+
+python ./tutorial/env_checkup.py
+
