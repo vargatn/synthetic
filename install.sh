f3686150feb31386175d56dcade7e565790e40b9
Tamas Norbert Varga <vargatn@gmail.com>
2023-02-20 11:43:50 +0100

    updated env

--- install.sh
+++ install.sh
@@ -2,17 +2,21 @@
 # this script must be run in interactive mode
 # bash -i install.sh
 
-conda create -y -n synth python=3.9 anaconda
+#conda create -y -n synth python=3.9
+conda env create -f base_env.yml
 
 conda activate synth
 
 conda env list
 
-conda install -y -c conda-forge ngmix=1.3.9 # this is a really complex package, if this installs, probably everything else works
+conda install pip
+
+conda install -y -c conda-forge ngmix=1.3.9
 conda install -y -c conda-forge galsim
 conda install -y -c conda-forge fitsio
 conda install -y -c conda-forge esutil
 conda install -y -c conda-forge healpy
+conda install -y -c conda-forge scikit-learn
 
 pip install git+https://github.com/esheldon/images.git \
             git+https://github.com/esheldon/meds \
