161b5b6edbc8dde61ba2c56edc1e9633beb729df
Tamas Norbert Varga <vargatn@gmail.com>
2023-09-18 19:13:46 +0200

    adding documentation coverage file

--- synthetic/STATUS.md
+++ synthetic/STATUS.md
@@ -4,7 +4,23 @@
 
 ## Documentation
 
-    tools.py 100%
+    tools.py        100%
+    mcal.py         0%
+    emulator
+    ----/cv.py        0%
+    ----/emulator.py  0%
+    ----/indexer.py   0%
+    ----/kde.py       0%
+    ----/multiply.py  0%
+    ----/reader.py    0%
+    render
+    ----/frame.py      0%
+    ----/generator.py  0%
+    ----/icl.py        0%
+    ----/render.py     0%
+    ----/shear.py      0%
+    ----/transforms.py 0%
+
 
 
 ## Test coverage
--- synthetic/mcal.py
+++ synthetic/mcal.py
@@ -17,6 +17,8 @@ from astropy.table import Table, vstack, hstack
 import fitsio as fio
 import multiprocessing as mp
 
+from tools import partition
+
 
 def infomaker(maxnum, medsfile, outfile_root, nchunk=100):
     lst = partition(np.arange(maxnum), n=nchunk)
@@ -366,43 +368,43 @@ def multi_mcal(infodicts, nprocess=1):
         pool.close()
         pool.join()
 
-
-def partition(lst, n):
-    """
-    Divides a list into N roughly equal chunks
-    Examples
-    --------
-    Define some test list, and look at the obtained chunks with different :code:`n` values::
-        >>> lst = np.arange(20)
-        >>> lst
-        array([ 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16,
-        17, 18, 19])
-        >>> partition(lst, n=5)
-        [array([0, 1, 2, 3]),
-         array([4, 5, 6, 7]),
-         array([ 8,  9, 10, 11]),
-         array([12, 13, 14, 15]),
-         array([16, 17, 18, 19])]
-        >>> partition(lst, n=6)
-        [array([0, 1, 2]),
-         array([3, 4, 5, 6]),
-         array([7, 8, 9]),
-         array([10, 11, 12]),
-         array([13, 14, 15, 16]),
-         array([17, 18, 19])]
-    As we can see, even when :code:`n` is not a divisor of :code:`len(lst)`, it returns
-    roughly balanced chunks
-    Parameters
-    ----------
-    lst : list
-        list to split up
-    n : int
-        chunks to make
-    Returns
-    -------
-    list of lists
-        list of chunks
-    """
-    division = len(lst) / float(n)
-    return [lst[int(round(division * i)): int(round(division * (i + 1)))]
-            for i in range(n) ]
+#
+# def partition(lst, n):
+#     """
+#     Divides a list into N roughly equal chunks
+#     Examples
+#     --------
+#     Define some test list, and look at the obtained chunks with different :code:`n` values::
+#         >>> lst = np.arange(20)
+#         >>> lst
+#         array([ 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16,
+#         17, 18, 19])
+#         >>> partition(lst, n=5)
+#         [array([0, 1, 2, 3]),
+#          array([4, 5, 6, 7]),
+#          array([ 8,  9, 10, 11]),
+#          array([12, 13, 14, 15]),
+#          array([16, 17, 18, 19])]
+#         >>> partition(lst, n=6)
+#         [array([0, 1, 2]),
+#          array([3, 4, 5, 6]),
+#          array([7, 8, 9]),
+#          array([10, 11, 12]),
+#          array([13, 14, 15, 16]),
+#          array([17, 18, 19])]
+#     As we can see, even when :code:`n` is not a divisor of :code:`len(lst)`, it returns
+#     roughly balanced chunks
+#     Parameters
+#     ----------
+#     lst : list
+#         list to split up
+#     n : int
+#         chunks to make
+#     Returns
+#     -------
+#     list of lists
+#         list of chunks
+#     """
+#     division = len(lst) / float(n)
+#     return [lst[int(round(division * i)): int(round(division * (i + 1)))]
+#             for i in range(n) ]
